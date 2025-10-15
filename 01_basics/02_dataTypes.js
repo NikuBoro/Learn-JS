865e4d519f822686f3d8dde2db53e0a63e154d08
noob03 <146568618+NikuBoro@users.noreply.github.com>
2025-10-15 08:19:14 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert() we r using node js not browser
+
+//code readability should be high
+console.log("Nikz");
+
+let name = "nikz"
+let age = 23
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigInt 
+//string => ""
+//boolean
+//null => standAlone value
+//undefined
+//symbol => used in uniqueness of components
+
+//object
+
+console.log(typeof null); //object
