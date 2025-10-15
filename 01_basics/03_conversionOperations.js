731305f6e4274fd3270c57719354b2d4cb0b0581
noob03 <146568618+NikuBoro@users.noreply.github.com>
2025-10-15 09:01:44 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = "33ab";
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"22"=> 22
+//"1abc"=> NaN
+//true => 1; false=> 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//""=> false
+//"nick"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
