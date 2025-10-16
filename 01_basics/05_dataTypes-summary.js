672e0b8aaed66c584d92c57638da85aa70de085f
NikuBoro <nikubasumatary6@gmail.com>
2025-10-16 20:09:32 +0530

    datatype summary

--- 01_basics/05_dataTypes-summary.js
+++ 01_basics/05_dataTypes-summary.js
@@ -1,2 +1,36 @@
-const n = "nikx";
-console.log(n);
+// Primitive
+
+// 7 types : String, Num, Bool, null, undefinded, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null 
+let userEmail ;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 12334444444444456667899n
+
+
+//Reference (non primitive)
+
+//Array , Objects, Functions
+
+const heros = ["sman", "nagraj", "doga"] 
+
+let myObj = {
+  name: "nikx",
+  age: 23
+}
+
+const myFun = function(){
+  console.log("hello !")
+}
+
+console.log(typeof outsideTemp) //prints null
+console.log(typeof myFun) // object function
