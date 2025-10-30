93be9976e6590ecad6e0b2a1452364b4dc4b7460
NikuBoro <nikubasumatary6@gmail.com>
2025-10-30 11:13:00 +0530

    practices

---
+++ 06NumberMethods/practices.js
@@ -0,0 +1,20 @@
+//How can you check if a number is an integer using JavaScript?
+let x = 8;
+console.log(Number.isInteger(x));
+
+
+// What happens when you use parseInt on a string with spaces?
+let y = " 123xyz";
+console.log(parseInt(y))
+
+//How does the toString() method work on a number?
+let z = 123;
+console.log(z.toString());
+
+//Can you convert a boolean to a number using the Number() method?
+let a = true;
+a = Number(a);
+console.log(typeof(a));
+
+let b = false;
+console.log(Number(b));
